905daf5eb6ee6b9fc4ecf6b325506adc943a7056
JeremyHwc <jeremy_hwc@163.com>
2018-11-17 14:10:40 +0800

    开始着手分析OkHttp网络库

--- demo-okhttp/src/main/java/com/tencent/demo_okhttp/MainActivity.java
+++ demo-okhttp/src/main/java/com/tencent/demo_okhttp/MainActivity.java
@@ -1,13 +1,37 @@
 package com.tencent.demo_okhttp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void synRequest(){
+        OkHttpClient client=new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
+        Request request = new Request.Builder().url("http://www.baidu.com").get().build();
+        Call call = client.newCall(request);
+        try{
+            Response response = call.execute();
+            Log.i("OKHTTP",response.body().string());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- readme/OkHttp.md
+++ readme/OkHttp.md
@@ -1,3 +1,17 @@
 # OkHttp源码分析
 
-   
+## 一、OkHttp流程
+
+### 涉及内容：
+OkHttpClient     
+Request     
+RealCall        
+Dispatcher      
+Intercepters:
+    包括RetryAndFollowIntercepter,BridgeIntercepter,CacheIntercepter,ConnectIntercepter,CallServerInterceper
+### OkHttp同步方法总结：
+- 创建OkHttpClient和Request对象
+- 将Request封装成Call对象
+- 调用Call的execute()方法发送同步请求
+*注意：OkHttp发送同步请求后，就会进入阻塞状态，直到收到响应，也就是说当前线程发送同步请求后，就会进
+入阻塞状态，直到收到数据响应，异步请求会重新创建一个工作线程，不会阻塞当前的线程*

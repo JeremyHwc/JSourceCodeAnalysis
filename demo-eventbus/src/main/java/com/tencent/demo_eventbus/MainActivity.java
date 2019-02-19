4c04038304ea60775ec3013c70618a92a142eef2
JeremyHwc <jeremy_hwc@163.com>
2019-02-19 18:30:14 +0800

    增加sqlite数据库学习

--- demo-eventbus/src/main/java/com/tencent/demo_eventbus/MainActivity.java
+++ demo-eventbus/src/main/java/com/tencent/demo_eventbus/MainActivity.java
@@ -2,16 +2,28 @@ package com.tencent.demo_eventbus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EventBus.getDefault().post(null);
     }
 }
